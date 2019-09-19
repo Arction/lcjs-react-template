bb68f64018ee9042740df163c208294e52eb837c
Tab <d491405879@gmail.com>
2019-09-19 16:41:17 +0300

    Add the comments

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Chart from './Component/Chart'
 class App extends Component {
   constructor() {
       super()
+  // Define the data point to the chart in the props
       this.data = [{ x: 0, y: 0 },
       { x: 1, y: 7 },
       { x: 2, y: 3 },
@@ -12,6 +13,7 @@ class App extends Component {
   }
   render() {
     return (
+      // Render a div includes a "Chart" component and passing the data to the componet.
       <div >
         <Chart data={this.data}/>
       </div>
--- src/Component/Chart.js
+++ src/Component/Chart.js
@@ -4,22 +4,34 @@ import React, { Component } from 'react';
 class Chart extends Component {
     constructor(props) {
         super(props)
+        // Create the chartId for the container.
         this.chartId = Math.trunc(Math.random() * 100)
     }
-    
+    // Define the function which create a chart.
     createChart() {
+        // create a chartXY.
         this.chart = lightningChart().ChartXY({ containerId: this.chartId })
+        // Set the Title to the chart.
         this.chart.setTitle('Getting Started')
+        // Add the LineSeries to the chart.
         const lineSeries = this.chart.addLineSeries()
+        // Set the strokStyle for the LineSeries.
         lineSeries.setStrokeStyle((style) => style.setThickness(5))
+        // Add the data point to the chart.
         lineSeries.add(this.props.data)
     }
-
+    // Call the function to create the chart after rendering the DOM.
     componentDidMount() {
         this.createChart()
     }
-
+   // Dispose the chart after the component was removed from the DOM
+    componentWillUnmount(){
+        this.chart.dispose()
+    }
+   
+    // 
     render() {
+        // return a div includes chart.
         return (
             <div id={this.chartId} className='fill'></div>
         )
