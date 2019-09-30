d2663db9fc4cf4eaf7a35b73c521063910bec779
Ilkka Kuosmanen <ilkka.kuosmanen@arction.com>
2019-09-30 14:28:06 +0300

    Resolve #1 Improved comments

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Chart from './Component/Chart'
 class App extends Component {
   constructor() {
     super()
-    // Define the data point to the chart in the props
-    this.data = [{ x: 0, y: 0 },
-    { x: 1, y: 7 },
-    { x: 2, y: 3 },
-    { x: 3, y: 10 }]
+    // Define data points that will be provided to the chart
+    this.data = [
+      { x: 0, y: 0 },
+      { x: 1, y: 7 },
+      { x: 2, y: 3 },
+      { x: 3, y: 10 }
+    ]
   }
 
   render() {
     return (
-      // Render a div includes a "Chart" component and passing the data to the componet.
+      // Render a div which includes a "Chart" component and pass the data to the component.
       <div >
         <Chart data={this.data} />
       </div>
