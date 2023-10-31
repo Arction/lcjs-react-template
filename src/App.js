17e525dea38b5d7e08e50ad49023a9202a730fe7
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2023-10-31 12:46:00 +0200

    Remove LC host testing code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import { MyChartComponent } from "./Components/MyChartComponent";
 import { LCHost } from "./LC";
-import { useState } from "react";
 
 const genRandomData = () => {
   const data = [];
   let prev = 0;
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < 100_000; i += 1) {
     const y = prev + (Math.random() * 2 - 1);
     data.push(y);
     prev = y;
@@ -16,23 +15,24 @@ const genRandomData = () => {
 };
 
 function App() {
-  const [showChart, setShowChart] = useState(true);
-  const data = genRandomData();
-
-  const handleShowChart = () => {
-    setShowChart(!showChart);
-  };
-
+  const data1 = genRandomData();
+  const data2 = genRandomData();
   return (
     // NOTE: LCHost should be defined at the top of component tree, before any and all LCJS based components
     // This let's them share the same LC context for performance benefits.
-    <>
-      <LCHost>
-        <div>LCJS Host</div>
-        <button onClick={handleShowChart}>Button</button>
-        {showChart && <MyChartComponent data={data} />}
-      </LCHost>
-    </>
+    <LCHost>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="chart">
+            <MyChartComponent id="chart-1" data={data1} />
+          </div>
+          <div className="chart">
+            <MyChartComponent id="chart-2" data={data2} />
+          </div>
+        </header>
+      </div>
+    </LCHost>
   );
 }
 
